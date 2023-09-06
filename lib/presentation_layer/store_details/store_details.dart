bc46ff9dac4841cd6c8b672c14dd89462ac54c84
abdelhamid <amabdo275@gmail.com>
2023-09-06 07:55:39 +0300

    add views for app

---
+++ lib/presentation_layer/store_details/store_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashViewScreen extends StatefulWidget {
+  const SplashViewScreen({super.key});
+
+  @override
+  State<SplashViewScreen> createState() => _SplashViewScreenState();
+}
+
+class _SplashViewScreenState extends State<SplashViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
